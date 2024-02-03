69422d00c545cab5f7a03a51d107dfb08a6f59f0
VladislavOlhovik <alkhovikvlad@gmail.com>
2024-02-02 19:41:51 -0500

    add prettier/gh-pages/jest

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import './App.css'
+import './App.css';
 
 function App() {
-  
-
-  return (
-    <>
-      Little Lemon
-    </>
-  )
+  return <>Little Lemon</>;
 }
 
-export default App
+export default App;
